66cb46abaa9073dbd6ec8b6c04d9d3162a7f457f
khalifa-pendrops <dikee5200@gmail.com>
2024-01-28 22:53:36 +0100

    methods for mode, range, variance and standard deviation creadted

--- stat.js
+++ stat.js
@@ -10,7 +10,7 @@ class MyDescriptiveStatistics {
   }
 
   // method/function to calculate the median (middle value)
-  calculateMedian() {
+  calcMedian() {
     const sortedData = this.data.slice().sort((a, b) => a - b);
     const middleIndex = Math.floor(sortedData.length / 2);
 
@@ -19,7 +19,53 @@ class MyDescriptiveStatistics {
     } else {
       return sortedData[middleIndex];
     }
+  }
+
+  // method/function to calculate the mode (most frequent value)
+  calcMode() {
+    const frequencyMap = {};
+    this.data.forEach((value) => {
+      frequencyMap[value] = (frequencyMap[value] || 0) + 1;
+    });
+
+    let mode;
+    let maxFrequency = 0;
+
+    for (const value in frequencyMap) {
+      if (frequencyMap[value] > maxFrequency) {
+        mode = Number(value);
+        maxFrequency = frequencyMap[value];
+      }
+    }
+
+    return mode;
+  }
+
+  // Function to calculate the range (difference between max and min)
+  calculateRange() {
+    const max = Math.max(...this.data);
+    const min = Math.min(...this.data);
+    return max - min;
+  }
+
+  // method/function to calculate the variance
+  calculateVariance() {
+    const mean = this.calculateMean();
+    const squaredDifferences = this.data.map((value) =>
+      Math.pow(value - mean, 2)
+    );
+    const sumSquaredDifferences = squaredDifferences.reduce(
+      (acc, value) => acc + value,
+      0
+    );
+    return sumSquaredDifferences / this.data.length;
+  }
+
+  // method/function to calculate the standard deviation
+  calculateStandardDeviation() {
+    return Math.sqrt(this.calculateVariance());
     }
     
     
+
 }    
